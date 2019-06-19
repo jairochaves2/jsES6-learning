c2c43c9501b9026856165da7baf6f474b02a1e62
Jairo Chaves <jairochaves2@gmail.com>
2019-06-18 21:54:10 -0300

    Controller

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -1,7 +1,21 @@
 class NegociacaoController{
+    constructor(){
+        let $=document.querySelector.bind(document);
+        this._inputData=$("#data");
+        this._inputQuantidade=$("#quantidade");
+        this._inputValor=$("#valor");
+    }
+
+
     adiciona(event){
         event.preventDefault();
-        alert("chamei ação no controller");
+        
+        let negociacao=new Negociacao(
+            new Date(this._inputData.value.split('-')),
+            this._inputQuantidade.value,
+            this._inputValor.value  
+        );
+        console.log(negociacao);
 
     }
 }
