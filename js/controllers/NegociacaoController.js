2cc7d184f1890ab4f830ce07d5b13d5098e009d9
Jairo Chaves <jairochaves2@gmail.com>
2019-06-24 14:41:27 -0300

    Data com array

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -11,7 +11,15 @@ class NegociacaoController{
         event.preventDefault();
         
         let negociacao=new Negociacao(
-            new Date(this._inputData.value.split('-')),
+            new Date(...this._inputData.value
+                .split('-')
+                .map(function(item,indice){
+                    if(indice==1){
+                        return item-1;
+                    }
+                    return item;
+                })
+            ),
             this._inputQuantidade.value,
             this._inputValor.value  
         );
