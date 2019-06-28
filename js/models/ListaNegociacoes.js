1892d64cb0f4dc5f02a04e1b40bb09414f3b6306
Jairo Chaves <jairochaves2@gmail.com>
2019-06-28 13:14:14 -0300

    Apaga negociacao

--- js/models/ListaNegociacoes.js
+++ js/models/ListaNegociacoes.js
@@ -8,4 +8,7 @@ class ListaNegociacoes{
     get negociacoes(){
         return [].concat(this._negociacoes);
     }
+    esvazia(){
+        this._negociacoes=[];
+    }
 }
--- js/views/NegociacoesView.js
+++ js/views/NegociacoesView.js
@@ -1,8 +1,8 @@
-class NegociacoesView{
+class NegociacoesView extends View{
     constructor(elemento){
-        this._elemento=elemento;
+        super(elemento);
     }
-    _template(model){
+    template(model){
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -38,9 +38,6 @@ class NegociacoesView{
                 </td>
             </tfoot>
         </table>`;
-
     }
-    update(model){
-        this._elemento.innerHTML=this._template(model);
-    }
+    
 }
