dbe7f71719017e53a6ee337e7a44e54e890533e2
Jairo Chaves <jairochaves2@gmail.com>
2019-06-28 14:53:58 -0300

    Proxy de objetos

--- js/models/ListaNegociacoes.js
+++ js/models/ListaNegociacoes.js
@@ -1,22 +1,18 @@
 class ListaNegociacoes{
-    constructor(armadilha){
+    constructor(){
         this._negociacoes=[]
-        this._armadilha=armadilha;
+        
     }
     adiciona(negociacoes){
         this._negociacoes.push(negociacoes);
-        this._armadilha(this);
-        
-        // Reflect.apply(this._armadilha,this.contexto,[this]);//(função_que_será_executada,contexto,parametros passados para a função_que_será_executada)
-
+          
     }
     get negociacoes(){
         return [].concat(this._negociacoes);
     }
     esvazia(){
         this._negociacoes=[];
-        this._armadilha(this);
-        //Reflect.apply(this._armadilha,this.contexto,[this]);//(função_que_será_executada,contexto,parametros passados para a função_que_será_executada)
-
+       
+        
     }
 }
