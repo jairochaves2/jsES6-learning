6e04efd4bf295b970c784be79def49c630677634
Jairo Chaves <jairochaves2@gmail.com>
2019-07-01 16:49:09 -0300

    Ajax puro

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -9,27 +9,54 @@ class NegociacaoController{
         this._inputValor=$("#valor");
 
         let self=this;
-        this._listaNegociacoes=new Proxy(new ListaNegociacoes(),{
-            get: function(target,prop,receiver){
-                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop])==typeof(Function)){
-                    return function(){
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop],target,arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target,prop,receiver);
-            }
-          });
-
         this._negociacoesView=new NegociacoesView($('#negociacoesView'));
         
-        this._mensagem=new Mensagem();
+        this._listaNegociacoes = new Bind (
+                new ListaNegociacoes(),
+                this._negociacoesView,
+                'adiciona', 'esvazia');
+
+                
         this._mensagemView=new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            this._mensagemView,
+            'texto');
     }
 
+    importaNegociacoes(){
+        let xhr=new XMLHttpRequest();
+        xhr.open('GET','negociacoes/semana');
+        xhr.onreadystatechange=()=>{
+            /*
+            0: requisição ainda não iniciada
 
+            1: conexão com o servidor estabelecida
+
+            2: requisição recebida
+
+            3: processando requisição
+
+            4: requisição está concluída e a resposta está pronta
+            */
+            if(xhr.readyState == 4){
+                if(xhr.status==200){
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto='não foi possível pegar as negociacoes do servidor';
+
+                }else{
+                    console.log(xhr.responseText)
+                    this._mensagem.texto='Obtendo as negociacoes do servidor';
+                }
+
+            }
+        };
+
+        xhr.send();
+
+    }
     adiciona(event){
         let log=console.log;
         event.preventDefault();
